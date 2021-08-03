a4fae67e56537e94e393903dd6e033a630c55be4
navigaid <navigaid@gmail.com>
2021-08-03 14:30:43 +0800

    add husky prettier

--- index.ts
+++ index.ts
@@ -1,33 +1,43 @@
 #!/usr/bin/env ts-node
 
-import { loadObject, loadText } from './utils';
-import { showVersion, showHelp, demoArgv, demoDump, demoEcho, demoSleep } from './cmds';
+import { loadObject, loadText } from "./utils";
+import {
+  showVersion,
+  showHelp,
+  demoArgv,
+  demoDump,
+  demoEcho,
+  demoSleep,
+} from "./cmds";
 
-async function main(): Promise<void>{
-	switch (process.argv[2]) {
-	case 'argv':
-		demoArgv();
-		break;
-	case 'dump':
-		demoDump();
-		break;
-	case 'echo':
-		demoEcho(process.argv.slice(3));
-		break;
-	case 'sleep':
-		await demoSleep(process.argv[3]);
-		break;
-	case 'version':
-	case '-v':
-	case '--version':
-		await showVersion();
-		break;
-	case 'help':
-	case '-h':
-	case '--help':
-	default:
-		showHelp();
-	}
+async function main(): Promise<void> {
+  const opt = fromArgs(Opt);
+  console.log(opt);
+
+  switch (process.argv[2]) {
+    case "argv":
+      demoArgv();
+      break;
+    case "dump":
+      demoDump();
+      break;
+    case "echo":
+      demoEcho(process.argv.slice(3));
+      break;
+    case "sleep":
+      await demoSleep(process.argv[3]);
+      break;
+    case "version":
+    case "-v":
+    case "--version":
+      await showVersion();
+      break;
+    case "help":
+    case "-h":
+    case "--help":
+    default:
+      showHelp();
+  }
 }
 
 void main();
