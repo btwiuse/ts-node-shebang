f1c9ed94df704cb1790b7eef1caed09e13abeadc
navigaid <navigaid@gmail.com>
2021-08-05 17:35:58 +0800

    refactor with commander
    ref: https://github.com/w3f/injection-tool/blob/master/src/index.ts

--- actions.ts
+++ actions.ts
@@ -1,61 +1,75 @@
 #!/usr/bin/env ts-node
 
-import fs from 'fs';
-import path from 'path';
-import { loadText } from './utils';
-import fetch from 'node-fetch';
-import Package from './package.json';
+import fs from "fs";
+import path from "path";
+import { loadText, sleep } from "./utils";
+import fetch from "node-fetch";
+import Package from "./package.json";
 
 if (!globalThis.fetch) {
-	globalThis.fetch = fetch;
+  globalThis.fetch = fetch;
 }
 
-export const ACTIONS = [
-	"argv",
-	"dump",
-	"echo",
-	"sleep",
-	"help",
-	"version",
-];
-
-export async function showVersion(){
-	console.log('current version:', Package.version);
-	await showLatest();
+export async function showVersion() {
+  console.log("current version:", Package.version);
+  await showLatest();
 }
 
-export async function showLatest(){
-	const resp = await fetch('https://registry.npmjs.org/-/package/@btwiuse/ts-node-shebang/dist-tags');
-	const body = await resp.json();
-	console.log('latest version:', body.latest);
+export async function showLatest() {
+  const resp = await fetch(
+    "https://registry.npmjs.org/-/package/@btwiuse/ts-node-shebang/dist-tags"
+  );
+  const body = await resp.json();
+  console.log("latest version:", body.latest);
 }
 
-export function showHelp(){
-	console.log('list of demo sub commands:');
-	for (let [i, arg] of ACTIONS.entries()) {
-		console.log('-', arg);
-	}
-	console.log('Please go to https://github.com/btwiuse/ts-node-shebang for instructions');
+export function showHelp() {
+  console.log("list of demo sub commands:");
+  for (let [i, arg] of ACTIONS.entries()) {
+    console.log("-", arg);
+  }
+  console.log(
+    "Please go to https://github.com/btwiuse/ts-node-shebang for instructions"
+  );
 }
 
-export function demoDump(){
-	console.log(loadText(__filename));
+export function demoMain() {
+  console.log(process.argv[1]);
 }
 
-export function demoEcho(args: string[]){
-	let filename = process.argv[1];
-	console.log(args.join(' '));
+export function demoWhere() {
+  console.log(__dirname);
 }
 
-export function demoArgv(){
-	console.log('process.argv:');
-	for (let [i, arg] of process.argv.entries()) {
-		console.log(i, arg);
-	}
+export function demoWhich() {
+  console.log(__filename);
 }
 
-export async function demoSleep(sec?: number = 1){
-	console.log(`${new Date()} sleep for ${sec} sec`);
-	await sleep(sec * 1000);
-	console.log(`${new Date()} recovered from sleep`);
+export function demoDump() {
+  console.log(loadText(__filename));
+}
+
+export function demoEcho(args: string[]) {
+  let filename = process.argv[1];
+  console.log(args.join(" "));
+}
+
+export function demoArgs(args: string[]) {
+  console.log("program.args:");
+  for (let [i, arg] of args.entries()) {
+    console.log(i, arg);
+  }
+}
+
+export function demoArgv() {
+  console.log("process.argv:");
+  for (let [i, arg] of process.argv.entries()) {
+    console.log(i, arg);
+  }
+}
+
+export async function demoSleep(sec?: number = 1) {
+  console.log(`${new Date()} sleep for ${sec} sec`);
+  await sleep(sec * 1000);
+  console.log(`${new Date()} recovered from sleep`);
 }
--- index.ts
+++ index.ts
@@ -4,37 +4,86 @@ import program, { Command } from "commander";
 import {
   showVersion,
   showHelp,
+  demoWhich,
+  demoWhere,
+  demoMain,
   demoArgv,
+  demoArgs,
   demoDump,
   demoEcho,
   demoSleep,
 } from "./actions";
 
 async function main(): Promise<void> {
-  switch (process.argv[2]) {
-    case "argv":
+  program
+    .command("version")
+    .description("show version")
+    .action(() => {
+      showVersion();
+    });
+  program
+    .command("args")
+    .description("show program.args")
+    .action(() => {
+      demoArgs(program.args);
+    });
+  program
+    .command("which")
+    .description("show self filepath")
+    .action(() => {
+      demoWhich();
+    });
+  program
+    .command("main")
+    .description("show main filepath")
+    .action(() => {
+      demoMain();
+    });
+  program
+    .command("where")
+    .description("show self dirname")
+    .action(() => {
+      demoWhere();
+    });
+  program
+    .command("argv")
+    .description("show process.argv")
+    .action(() => {
       demoArgv();
-      break;
-    case "dump":
+    });
+  program
+    .command("dump")
+    .description("dump self content")
+    .action(() => {
       demoDump();
-      break;
-    case "echo":
-      demoEcho(process.argv.slice(3));
-      break;
-    case "sleep":
-      await demoSleep(process.argv[3]);
-      break;
-    case "version":
-    case "-v":
-    case "--version":
-      await showVersion();
-      break;
-    case "help":
-    case "-h":
-    case "--help":
-    default:
-      showHelp();
-  }
+    });
+  program
+    .command("echo")
+    .description("echo args")
+    .option("-n, --no-newline", "exclude trailing new line")
+    .arguments("[string...]")
+    .action((args, opts, command) => {
+      // console.log('args', args);
+      // console.log('opts', opts);
+      let line = args.join(" ");
+      if (opts.newline) {
+        console.log(line);
+      } else {
+        process.stdout.write(line);
+      }
+    });
+  program
+    .command("sleep")
+    .description("wait a few seconds")
+    .arguments("[number]")
+    .action((args, opts, command) => {
+      let sec = 1;
+      if (args) {
+        sec = args[0];
+      }
+      demoSleep(sec);
+    });
+  program.parse(process.argv);
 }
 
 void main();
